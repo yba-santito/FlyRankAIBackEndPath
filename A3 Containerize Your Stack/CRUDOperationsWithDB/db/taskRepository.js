5def09a8489d7a8cce10730b2fe56fd913b89d57
yba-santito <odsctradinggroup@gmail.com>
2026-08-06 21:43:04 +0200

    Stage 4: docker-compose the whole stack

--- A3 Containerize Your Stack/CRUDOperationsWithDB/db/taskRepository.js
+++ A3 Containerize Your Stack/CRUDOperationsWithDB/db/taskRepository.js
@@ -14,24 +14,24 @@ class TaskRepository{
     }
 
     async update(id, title, done){
-        const result = await this.db.run(
-            "Update Tasks SET Title = ?, done = ? where id = ?",
+        const result = await this.db.query(
+            "Update Tasks SET Title = $1, done = $2 where id = $3",
             [title, done, id]
         );
-        return result.changes > 0;
+        return result.rowCount > 0;
     }
 
     async create(title, done = false){
-        const result = await this.db.run(
-            "Insert Into Tasks (title, done) Values (?,?)",
+        const result = await this.db.query(
+            "Insert Into Tasks (title, done) Values ($1,$2)",
             [title, done]
         );
-        return {id: result.lastID, title, done};
+        return result.rows[0];
     }
 
     async delete(id) {
-        const result = await this.db.run('DELETE FROM Tasks WHERE id = ?', [id]);
-        return result.changes > 0;
+        const result = await this.db.query('DELETE FROM Tasks WHERE id = $1', [id]);
+        return result.rowCount > 0;
     }
 }
 
